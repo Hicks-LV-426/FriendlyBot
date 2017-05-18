d32ecb44168781879a50e6d6634355536a712b16
Aidas Pasilis <aidas.pasilis@intermedix.com>
2017-05-18 16:48:58 +0300

    calculations changes in evaluating cost of action

--- FighterBot/Program.cs
+++ FighterBot/Program.cs
@@ -19,13 +19,8 @@
       {
         universe.Refresh();
         fleet.Scan(universe);
-
-        // Write an action using Console.WriteLine()
-        // To debug: Console.Error.WriteLine("Debug messages...");
-
-
-        // Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
-        Console.WriteLine("WAIT");
+        fleet.Plan();
+        fleet.Execute();
       }
     }
 
@@ -114,40 +109,149 @@
     }
     class Fleet
     {
+      Universe Universe { get; set; }
       Dictionary<int, Ship> Ships = new Dictionary<int, Ship>();
 
       public void Scan(Universe universe)
       {
+        Universe = universe;
         foreach (var f in universe.Factories.Where(f => f.Value.Owner == Owner.Player))
         {
-          if (!Ships.ContainsKey(f.Key)) Ships.Add(f.Key, new Ship(f.Key));
+          if (!Ships.ContainsKey(f.Key)) Ships.Add(f.Key, new Ship(f.Key, universe.Links));
 
           var ship = Ships[f.Key];
           ship.Update(f.Value, universe.Troops);
         }
       }
+      public void Plan()
+      {
+        Defend();
+        CalculateActions();
+        ChooseActions();
+      }
+
+      private void Defend()
+      {
+        var needDefense = Ships.Where(s => s.Value.DefenseDeficit > 0).Select(sh => sh.Value);
+        if (needDefense.Count() == 0) return;
+
+        foreach (var ship in needDefense)
+        {
+          var defenders = Ships.Where(s => s.Value.ArmySize > 0).Select(s => s.Value).OrderBy(o => o.DistanceTo(ship.Id));
+          foreach (var defender in defenders)
+          {
+            defender.Defend(ship);
+          }
+        }
+
+      }
+      private void CalculateActions()
+      {
+        throw new NotImplementedException();
+      }
+      private void ChooseActions()
+      {
+        throw new NotImplementedException();
+      }
+
+      public void Execute()
+      {
+        // Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
+        Console.WriteLine("WAIT");
+      }
+      private void AddMove(int sourceId, int targetId, int size)
+      {
+        //
+      }
     }
     class Ship
     {
-      public Ship(int id)
+      public Ship(int id, List<Link> links)
       {
         Id = id;
+        foreach (var link in links.Where(l => l.HasFactory(Id)))
+        {
+          Distances.Add(link.GetOtherId(Id), link.Distance);
+        }
       }
       public int Id { get; private set; }
       public int Production { get; private set; } = 0;
       public int Population { get; private set; } = 0;
-      public int Defense { get; private set; } = 0;
-      public bool Frozen { get; private set; }
-      public int Army => Population - Defense;
-      List<Troop> Attackers = new List<Troop>();
+      public int DefenseDeficit { get; private set; } = 0;
+      public int FrozenTurnsRemaining { get; private set; }
+      public bool Frozen => FrozenTurnsRemaining > 0;
+      public int ArmySize { get; private set; }
+      List<Troop> IncommingTroops = new List<Troop>();
+      Dictionary<int, int> Distances = new Dictionary<int, int>();
+
       public void Update(Factory factory, List<Troop> troops)
       {
         Production = factory.Production;
         Population = factory.Cyborgs;
-        Frozen = factory.TurnsFrozen > 0;
+        FrozenTurnsRemaining = factory.TurnsFrozen;
 
-        Attackers.Clear();
-        Attackers.AddRange(troops.Where(t => t.TargetFactoryId == Id));
+        IncommingTroops.Clear();
+        IncommingTroops.AddRange(troops.Where(t => t.TargetFactoryId == Id));
+
+        CalculateDefense();
+      }
+
+      private void CalculateDefense()
+      {
+        var turn = 0;
+        var population = Population;
+        var army = 0;
+        var deficit = 0;
+
+        var incomming = IncommingTroops.OrderBy(a => a.Turns);
+        foreach (var fighter in incomming)
+        {
+          var frozenTurnsRemaining = Frozen && turn <= FrozenTurnsRemaining ? FrozenTurnsRemaining - turn : 0;
+
+          var turnsTillImpact = fighter.Turns - turn;
+          turn += turnsTillImpact;
+
+          if (turnsTillImpact < frozenTurnsRemaining) frozenTurnsRemaining = turnsTillImpact;
+
+          population += Production * (turnsTillImpact - frozenTurnsRemaining);
+
+          if (fighter.Owner == Owner.Player) population += fighter.Cyborgs;
+          else population -= fighter.Cyborgs;
+
+          if (population < 0)
+          {
+            var currentDeficit = Math.Abs(population) + 1;
+
+            if (army >= currentDeficit)
+            {
+              army -= currentDeficit;
+            }
+            else
+            {
+              deficit += (currentDeficit - army);
+              population += (currentDeficit - army);
+              army = 0;
+            }
+          }
+          if (population > 1)
+          {
+            army += population - 1;
+          }
+        }
+
+        DefenseDeficit = deficit;
+        ArmySize = army;
+      }
+      public int DistanceTo(int factoryId) => Distances[factoryId];
+
+      internal Action Defend(Ship ship)
+      {
+        var cyborgs = ArmySize;
+        if (ArmySize >= ship.DefenseDeficit) cyborgs = ArmySize - ship.DefenseDeficit;
+
+        ArmySize -= cyborgs;
+        ship.DefenseDeficit -= cyborgs;
+        return new Action().Move(this, ship, cyborgs);
       }
     }
 
@@ -156,6 +260,12 @@
       public int Factory1Id { get; set; }
       public int Factory2Id { get; set; }
       public int Distance { get; set; }
+      public bool HasFactory(int factoryId) => Factory1Id == factoryId || Factory2Id == factoryId;
+      public int GetDistance(int id1, int id2)
+      {
+        return HasFactory(id1) && HasFactory(id2) ? Distance : 0;
+      }
+      public int GetOtherId(int id) => Factory1Id == id ? Factory2Id : Factory1Id;
     }
     struct Factory
     {
@@ -180,6 +290,21 @@
       public int TargetFactoryId { get; set; }//arg3: identifier of the targeted factory if it's your bomb, -1 otherwise
       public int Turns { get; set; }//arg4: remaining number of turns before the bomb explodes(positive integer) if that's your bomb, -1 otherwise
       public int Arg5 { get; set; }//arg5: unused
+    }
+    class Action
+    {
+      public Action Move(Ship source, Ship target, int cyborgs)
+      {
+        Cost = cyborgs / (source.DistanceTo(target.Id) * target.Production);
+        Value = $"MOVE {source.Id} {target.Id} {cyborgs}";
+        // population increase  turns cyborgs  =(turns+cyborgs)/((population/turns) + increase)
+        // 0          3         10    15       3.333333333
+        // 0          1         1     10       0.215686275
+
+        return this;
+      }
+      public decimal Cost { get; private set; }
+      public string Value { get; private set; }
     }
     public enum Owner
     {
